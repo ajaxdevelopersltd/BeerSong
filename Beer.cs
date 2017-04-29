ae26bd75ef8bcaf34ebf35c86ca96fb51871c4f6
Andrew McDonald <andy@ajaxdevelopers.biz>
2017-04-29 19:38:17 +0100

    Optimised for readability

--- Beer.cs
+++ Beer.cs
@@ -9,45 +9,47 @@ public static class Beer
 
     public static string Verse(int number)
     {
-        var expected = "";
+        var verse = "";
+        var newline = "\n";
+
         if (number == 0)
         {
-            expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+            verse = "No more bottles of beer on the wall, no more bottles of beer." + newline + "Go to the store and buy some more, 99 bottles of beer on the wall." + newline;
         }
         else if (number == 1)
         {
-            expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+            verse = "1 bottle of beer on the wall, 1 bottle of beer." + newline + "Take it down and pass it around, no more bottles of beer on the wall." + newline;
         }
         else if (number == 2)
         {
-            expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
+            verse = "2 bottles of beer on the wall, 2 bottles of beer." + newline + "Take one down and pass it around, 1 bottle of beer on the wall." + newline;
         }
         else
         {
-            expected = number + " bottles of beer on the wall, " + number + " bottles of beer.\n" +
-            "Take one down and pass it around, " + (number - 1) + " bottles of beer on the wall.\n" +
+            verse = number + " bottles of beer on the wall, " + number + " bottles of beer." + newline +
+            "Take one down and pass it around, " + (number - 1) + " bottles of beer on the wall." + newline +
             "";
         }
 
-        return expected;
+        return verse;
 
     }
 
     public static string Verses(int begin, int end)
     {
 
-        var expected = "";
+        var verse = "";
         int i = begin;
 
         do
         {
-            expected = (expected + Verse(i) + "\n");            
+            verse = (verse + Verse(i) + "\n");            
 
             i--;
         }
         while (i >= end);       
 
-        return expected;
+        return verse;
     }
 
     public static string Sing(int start, int stop)
