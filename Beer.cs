13833f6155c55a20f37c58d0d9c9b7e0c836f8bf
Andrew McDonald <andy@ajaxdevelopers.biz>
2017-04-29 15:02:11 +0100

    Version 1

---
+++ Beer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+public static class Beer
+{
+
+    public static string Verse(int number)
+    {
+        var expected = "";
+        if (number == 0)
+        {
+            expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+        }
+        else if (number == 1)
+        {
+            expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+        }
+        else if (number == 2)
+        {
+            expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
+        }
+        else
+        {
+            expected = number + " bottles of beer on the wall, " + number + " bottles of beer.\n" +
+            "Take one down and pass it around, " + (number - 1) + " bottles of beer on the wall.\n" +
+            "";
+        }
+
+        return expected;
+
+    }
+
+    public static string Verses(int begin, int end)
+    {
+
+        var expected = "";
+        int i = begin;
+
+        do
+        {
+            expected = (expected + Verse(i) + "\n");            
+
+            i--;
+        }
+        while (i >= end);       
+
+        return expected;
+    }
+
+    public static string Sing(int start, int stop)
+    {
+        return Verses(start, stop);
+    }
+
+}
